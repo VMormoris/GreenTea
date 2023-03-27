511baaac258178847a2bcd3b4d36c44915390768
VMormoris <vmormoris2014@gmail.com>
2023-03-27 15:00:23 +0200

    Adding more type aliases inspired by Rust

--- GreenTea/src/Engine/Core/Engine.h
+++ GreenTea/src/Engine/Core/Engine.h
@@ -44,10 +44,12 @@
 #include <cstdint>
 
 //Type definitions
+typedef std::int8_t int8;
 typedef std::int16_t int16;
 typedef std::int32_t int32;
 typedef std::int64_t int64;
 
+typedef std::uint8_t uint8;
 typedef std::uint16_t uint16;
 typedef std::uint32_t uint32;
 typedef std::uint64_t uint64;
@@ -56,6 +58,22 @@ typedef float float32;
 typedef double float64;
 
 typedef unsigned char byte;
+
+//Rust-like types
+using i8 = int8;
+using i16 = int16;
+using i32 = int32;
+using i64 = int64;
+
+using u8 = uint8;
+using u16 = uint16;
+using u32 = uint32;
+using u64 = uint64;
+
+using f32 = float32;
+using f64 = float64;
+
+#define let const auto
 
 //Reflection macros
 #ifndef REFLECTION
